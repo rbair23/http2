bb883772357a3b3341a20a3c50e6e3d0d4c7fdd9
Richard Bair <rbair23@users.noreply.github.com>
2022-10-05 20:37:36 -0700

    Add HPACK library
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("com.google.code.findbugs:jsr305:3.0.2")
+    implementation("com.twitter:hpack:1.0.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testImplementation("com.squareup.okhttp3:okhttp:4.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
